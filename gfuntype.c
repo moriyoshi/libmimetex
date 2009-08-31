f4244cdea59f9861c4a715311db537186ca0c955
Moriyoshi Koizumi <mozo@mozo.jp>
2009-09-01 04:08:18 +0900

    * Move the content of cstruct.c into gfuntype.c.

--- gfuntype.c
+++ gfuntype.c
@@ -95,6 +95,102 @@ static	int isrepeat = ISREPEAT;	/* true to store line repeat counts*/
 #define	TYPECAST    "(pixbyte *)"	/* typecast for pixmap string */
 
 /* ==========================================================================
+ * Function:    cstruct_raster ( rp, fp, col1 )
+ * Purpose: Emit a C struct of rp on fp, starting in col1.
+ * --------------------------------------------------------------------------
+ * Arguments:   rp (I)      ptr to raster struct for which
+ *              a C struct is to be generated.
+ *      fp (I)      File ptr to output device (defaults to
+ *              stdout if passed as NULL).
+ *      col1 (I)    int containing 0...65; output lines
+ *              are preceded by col1 blanks.
+ * --------------------------------------------------------------------------
+ * Returns: ( int )     1 if completed successfully,
+ *              or 0 otherwise (for any error).
+ * --------------------------------------------------------------------------
+ * Notes:
+ * ======================================================================= */
+/* --- entry point --- */
+static int cstruct_raster(raster *rp, FILE *fp, int col1)
+{
+    /* ------------------------------------------------------------
+    Allocations and Declarations
+    ------------------------------------------------------------ */
+    /* field within output line */
+    char    field[64];
+    /* type cast for pixmap string */
+    char    typecast[64] = "(pixbyte *)";
+    /* to emit raster bitmap */
+    int hex_bitmap();
+    /* emit a string and comment */
+    int emit_string();
+    /* ------------------------------------------------------------
+    emit width and height
+    ------------------------------------------------------------ */
+    sprintf(field, "{ %2d,  %3d,%2d,%2d, %s\n", /* format width,height,pixsz */
+            rp->width, rp->height, rp->format, rp->pixsz, typecast);
+    emit_string(fp, col1, field, "width,ht, fmt,pixsz,map...");
+    /* ------------------------------------------------------------
+    emit bitmap and closing brace, and return to caller
+    ------------------------------------------------------------ */
+    /* emit bitmap */
+    hex_bitmap(rp, fp, col1 + 2, 1);
+    /* emit closing brace */
+    emit_string(fp, 0, " }", NULL);
+    /* back to caller with 1=okay, 0=failed */
+    return (1);
+} /* --- end-of-function cstruct_raster() --- */
+
+/* ==========================================================================
+ * Function:    cstruct_chardef ( cp, fp, col1 )
+ * Purpose: Emit a C struct of cp on fp, starting in col1.
+ * --------------------------------------------------------------------------
+ * Arguments:   cp (I)      ptr to chardef struct for which
+ *              a C struct is to be generated.
+ *      fp (I)      File ptr to output device (defaults to
+ *              stdout if passed as NULL).
+ *      col1 (I)    int containing 0...65; output lines
+ *              are preceded by col1 blanks.
+ * --------------------------------------------------------------------------
+ * Returns: ( int )     1 if completed successfully,
+ *              or 0 otherwise (for any error).
+ * --------------------------------------------------------------------------
+ * Notes:
+ * ======================================================================= */
+/* --- entry point --- */
+static int cstruct_chardef(chardef *cp, FILE *fp, int col1)
+{
+    /* ------------------------------------------------------------
+    Allocations and Declarations
+    ------------------------------------------------------------ */
+    /* field within output line */
+    char    field[64];
+    /* ------------------------------------------------------------
+    emit   charnum, location, name  /  hirow, hicol,  lorow, locol
+    ------------------------------------------------------------ */
+    /* --- charnum, location, name --- */
+    /*char#,location*/
+    sprintf(field, "{ %3d,%5d,\n", cp->charnum, cp->location);
+    emit_string(fp, col1, field, "character number, location");
+    /* --- toprow, topleftcol,   botrow, botleftcol  --- */
+    sprintf(field, "  %3d,%2d,  %3d,%2d,\n",    /* format... */
+            cp->toprow, cp->topleftcol,           /* toprow, topleftcol, */
+            /* and botrow, botleftcol */
+            cp->botrow, cp->botleftcol);
+    emit_string(fp, col1, field, "topleft row,col, and botleft row,col");
+    /* ------------------------------------------------------------
+    emit raster and chardef's closing brace, and then return to caller
+    ------------------------------------------------------------ */
+    /* emit raster */
+    cstruct_raster(&cp->image, fp, col1 + 4);
+    /* emit closing brace */
+    emit_string(fp, 0, "  }", NULL);
+    /* back to caller with 1=okay, 0=failed */
+    return (1);
+} /* --- end-of-function cstruct_chardef() --- */
+
+
+/* ==========================================================================
  * Function:	main() for gfuntype.c
  * Purpose:	interprets command-line args, etc
  * --------------------------------------------------------------------------
